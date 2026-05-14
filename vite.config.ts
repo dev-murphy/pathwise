6ce77143fa5095030c7f722d35fd9927dca30c50
dev-murphy <facey.murphy1@gmail.com>
2026-05-13 19:44:56 -0500

    chore: add alias to app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
+
+import Tailwindcss from "@tailwindcss/vite";
+import Vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [Vue(), Tailwindcss()],
+  resolve: {
+    alias: {
+      "~": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
