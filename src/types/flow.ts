7993febec2aa62ef66bc553876b3866ac9cba790
dev-murphy <facey.murphy1@gmail.com>
2026-05-14 14:37:46 -0500

    feat(types): add targetNodeId to FieldOption, add multiselect to MULTI_BRANCH_TYPES

--- src/types/flow.ts
+++ src/types/flow.ts
@@ -18,7 +18,7 @@ export const FIELD_TYPE_LABELS: Record<FieldType, string> = {
 }
 
 /** Field types that support multiple outgoing branches */
-export const MULTI_BRANCH_TYPES: FieldType[] = ['dropdown', 'radio']
+export const MULTI_BRANCH_TYPES: FieldType[] = ['dropdown', 'radio', 'multiselect']
 
 /** Field types that have a list of selectable options */
 export const OPTIONS_TYPES: FieldType[] = ['dropdown', 'radio', 'multiselect']
@@ -26,6 +26,7 @@ export const OPTIONS_TYPES: FieldType[] = ['dropdown', 'radio', 'multiselect']
 export interface FieldOption {
   id: string
   label: string
+  targetNodeId?: string
 }
 
 export interface QuestionNodeData {
